207f819d302fbed56506005b02e8615a61fff40f
bala-amypo <bala@amypo.in>
2025-12-18 08:29:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/SpendCategoryController.java
+++ src/main/java/com/example/demo/controller/SpendCategoryController.java
@@ -15,19 +15,19 @@ public class SpendCategoryController {
     @Autowired
     SpendCategoryService categoryService;
 
-    // POST /api/categories
+   
     @PostMapping
     public SpendCategory create(@RequestBody SpendCategory category) {
         return categoryService.createCategory(category);
     }
 
-    // PUT /api/categories/{id}
+   
     @PutMapping("/{id}")
     public SpendCategory update(@PathVariable Long id, @RequestBody SpendCategory category) {
         return categoryService.updateCategory(id, category);
     }
 
-    // GET /api/categories/{id}
+   
     @GetMapping("/{id}")
     public SpendCategory getById(@PathVariable Long id) {
         return categoryService.getCategoryById(id);
