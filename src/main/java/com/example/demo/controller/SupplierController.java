62d98fa05d406feb236c729588a33b5cd80ebb4f
bala-amypo <bala@amypo.in>
2025-12-18 18:15:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/SupplierController.java
+++ src/main/java/com/example/demo/controller/SupplierController.java
@@ -3,6 +3,14 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.demo.entity.Supplier;
 import com.example.demo.service.SupplierService;
 
@@ -11,24 +19,28 @@ import com.example.demo.service.SupplierService;
 public class SupplierController {
 
     @Autowired
-    SupplierService service;
+    private SupplierService service;
 
+ 
     @PostMapping
     public Supplier create(@RequestBody Supplier supplier) {
         return service.createSupplier(supplier);
     }
 
+  
     @PutMapping("/{id}")
     public Supplier update(@PathVariable Long id,
                            @RequestBody Supplier supplier) {
         return service.updateSupplier(id, supplier);
     }
 
+  
     @GetMapping("/{id}")
     public Supplier getById(@PathVariable Long id) {
         return service.getSupplierById(id);
     }
 
+   
     @GetMapping
     public List<Supplier> getAll() {
         return service.getAllSuppliers();
