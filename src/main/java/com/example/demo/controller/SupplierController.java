56892e65e2b391d95c553b03afc0559077f687fe
bala-amypo <bala@amypo.in>
2025-12-21 03:52:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/SupplierController.java
+++ src/main/java/com/example/demo/controller/SupplierController.java
@@ -2,52 +2,38 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.Entity.Supplier;
+import com.example.demo.entity.Supplier;
 import com.example.demo.service.SupplierService;
 
 @RestController
-@RequestMapping("/api/suppliers")
+@RequestMapping("/suppliers")
 public class SupplierController {
 
-    @Autowired
-    private SupplierService service;
+    private final SupplierService supplierService;
 
- 
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
     @PostMapping
-    public Supplier create(@RequestBody Supplier supplier) {
-        return service.createSupplier(supplier);
+    public Supplier createSupplier(@RequestBody Supplier supplier) {
+        return supplierService.createSupplier(supplier);
     }
 
-  
-    @PutMapping("/{id}")
-    public Supplier update(@PathVariable Long id,
-                           @RequestBody Supplier supplier) {
-        return service.updateSupplier(id, supplier);
-    }
-
-  
     @GetMapping("/{id}")
-    public Supplier getById(@PathVariable Long id) {
-        return service.getSupplierById(id);
+    public Supplier getSupplier(@PathVariable Long id) {
+        return supplierService.getSupplierById(id);
     }
 
-   
     @GetMapping
-    public List<Supplier> getAll() {
-        return service.getAllSuppliers();
+    public List<Supplier> getAllSuppliers() {
+        return supplierService.getAllSuppliers();
     }
 
     @PutMapping("/{id}/deactivate")
-    public Supplier deactivate(@PathVariable Long id) {
-        return service.deactivateSupplier(id);
+    public void deactivateSupplier(@PathVariable Long id) {
+        supplierService.deactivateSupplier(id);
     }
 }
