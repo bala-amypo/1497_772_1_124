b1becadc8b85ac2aa4d81654e15f647ff209b138
bala-amypo <bala@amypo.in>
2025-12-22 01:28:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiversityClassificationController.java
+++ src/main/java/com/example/demo/controller/DiversityClassificationController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.DiversityClassification;
 import com.example.demo.service.DiversityClassificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -18,10 +19,22 @@ public class DiversityClassificationController {
         this.classificationService = classificationService;
     }
     
+    @PostMapping
+    public ResponseEntity<DiversityClassification> createClassification(@RequestBody DiversityClassification classification) {
+        DiversityClassification createdClassification = classificationService.createClassification(classification);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdClassification);
+    }
+    
     @GetMapping
     public ResponseEntity<List<DiversityClassification>> getAllClassifications() {
         List<DiversityClassification> classifications = classificationService.getAllClassifications();
         return ResponseEntity.ok(classifications);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<DiversityClassification> getClassificationById(@PathVariable Long id) {
+        DiversityClassification classification = classificationService.getClassificationById(id);
+        return ResponseEntity.ok(classification);
     }
     
     @PutMapping("/{id}/deactivate")
