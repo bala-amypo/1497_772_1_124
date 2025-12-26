8208d5a9ef8f9012e4a4c7d0db82c4e8583163dc
bala-amypo <bala@amypo.in>
2025-12-26 10:20:07 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,13 +1,17 @@
 package com.example.demo.servlet;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+
+@WebServlet(urlPatterns = "/simple-status")
 
 public class SimpleStatusServlet extends HttpServlet {
     
+   
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws IOException {
